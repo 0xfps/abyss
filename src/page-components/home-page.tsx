385d777a517bd65679defff06a39a210c12c17a1
0xfps <anthony.nnaemeka.umeh@gmail.com>
2025-10-21 11:05:53 +0100

    feat: bump package.

--- src/page-components/home-page.tsx
+++ src/page-components/home-page.tsx
@@ -3,9 +3,10 @@
 import { AnnouncementAndFetcher } from "@/components/announcement-and-fetcher"
 import NavBar from "@/components/nav-bar"
 
-export default function HomePage() {
+export default function HomePage() {;
+    
     return <>
         <NavBar />
-        <AnnouncementAndFetcher/>
+        <AnnouncementAndFetcher />
     </>
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,3 +3,4 @@ export const HOW_TO_USE = "how-to-use"
 export const LATEST_ANNOUNCEMENT = "announcement-afe453ef"
 export const DEPOSIT_EVENT = "DepositAdded(bytes32)"
 export const BLOCK_CRAWL_INTERVAL = 900
+export const SVG_IMG_PREPEND ="data:image/svg+xml;utf8,"
